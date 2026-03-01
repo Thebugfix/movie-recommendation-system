cc06dd99d270ff94a701e3f7e949e29ee6cfa2c2
Naved Shaikh <shaikhnaved0001@gmail.com>
2026-03-01 15:35:40 +0530

    update

--- frontend/src/services/favoriteApi.ts
+++ frontend/src/services/favoriteApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "https://movie-recommendation-system-ogcw.vercel.app/api/favorites";
+const API = `${import.meta.env.VITE_BACKEND_URL}/api/favorites`;
 
 export const saveMovie = (movie:any) =>
   axios.post(API, movie);
